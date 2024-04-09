02cb2265ed49bbf2069ba002cf49527fb9fe0d04
André S.R <adres.1481@gmail.com>
2024-04-09 16:47:05 -0300

    Terceira att

--- script/main.js
+++ script/main.js
@@ -1,26 +1,68 @@
 //VARIAVEIS
+const olElement = document.querySelector('#word-ol')
+const tryElement = document.querySelector('#try')
+tryElement
 var chosedWord = 0
+var remaingTry = 6
 
 //FUNCOES
+function randomWord() {
+    let number = Math.floor(Math.random() * wordList.length)
+    chosedWord = wordList[number]
+    console.log(chosedWord)
+}
+
+function creator() {
+    for (let i = 0; i < chosedWord.length; i++) {
+        let element = document.createElement('li')
+        element.textContent = chosedWord[i]
+        element.classList.add('unvisible')
+        olElement.appendChild(element)
+    }
+
+    tryElement.textContent = remaingTry
+}
+
 function eventListenerCreator() {
     for (let i = 97; i <= 122; i++) {
         let letra = String.fromCharCode(i);
         const element = document.querySelector(`#${letra}`)
         element.addEventListener('click', () => {
+            testLetter(letra)
             console.log(letra)
         })
-        //console.log(letra);
     }
 }
 
-function testLetter() {
-    
+function testLetter(letter) {
+    let testeRecord = false
+    for (let i = 0; i < chosedWord.length; i++) {
+        if (letter == chosedWord[i]) {
+            trueTest(letter)
+            testeRecord = true
+        } 
+    }
+    if (testeRecord === false) {
+        falseTeste(letter)
+    }
+    if (remaingTry === 0) {
+        // fail screen
+    }
 }
 
-function randomWord() {
-    let number = Math.floor(Math.random() * wordList.length)
-    chosedWord = wordList[number]
-    console.log(chosedWord)
+function trueTest(letter) {
+    console.log('é nois')
+    let letterElement = document.querySelector(`#${letter}`)
+    letterElement.classList.remove('unvisible')
+}
+
+function falseTeste(letter) {
+    remaingTry = remaingTry - 1
+    tryElement.textContent = remaingTry
+    let letterElement = document.querySelector(`#${letter}`)
+    letterElement.setAttribute('class', 'notclick')
+    letterElement.style.color = "red"
+    //resposta visual
 }
 
 //EVENTOS
@@ -30,3 +72,4 @@ function randomWord() {
 //START EVENTS
 eventListenerCreator()
 randomWord()
+creator()
--- script/word-list.js
+++ script/word-list.js
@@ -1,12 +1,12 @@
 const wordList = [
-    "cachorro", "gato", "elefante", "pássaro", "leão", "tigre", "macaco", "zebra", "girafa", "cobra",
+    "cachorro", "gato", "elefante", "passaro", "leao", "tigre", "macaco", "zebra", "girafa", "cobra",
     "peixe", "borboleta", "abelha", "formiga", "aranha", "caracol", "rato", "coelho", "coruja", "lontra",
     "cavalo", "vaca", "porco", "ovelha", "galinha", "pato", "cisne", "pombo", "coala", "canguru",
-    "papagaio", "jacaré", "crocodilo", "hipopótamo", "rinoceronte", "girassol", "margarida", "rosa", "lírio", "tulipa",
-    "sapo", "lagarto", "iguana", "lagartixa", "camaleão", "truta", "salmão", "bacalhau", "atum", "sardinha",
-    "morango", "banana", "abacaxi", "uva", "maçã", "laranja", "limão", "melancia", "melão", "kiwi",
-    "abacate", "mamão", "pêssego", "ameixa", "figo", "maracujá", "caju", "castanha", "noz", "avelã",
-    "pistache", "amendoim", "café", "chá", "chocolate", "leite", "suco", "refrigerante", "água", "cerveja",
-    "vinho", "whisky", "rum", "vodka", "tequila", "gin", "martini", "vermute", "sakê", "conhaque",
-    "churrasco", "sushi", "pizza", "hambúrguer", "sanduíche", "taco", "kebab", "curry", "sorvete", "bolo"
-]
+    "papagaio", "jacare", "crocodilo", "hipopotamo", "rinoceronte", "girassol", "margarida", "rosa", "lirio", "tulipa",
+    "sapo", "lagarto", "iguana", "lagartixa", "camaleao", "truta", "salmao", "bacalhau", "atum", "sardinha",
+    "morango", "banana", "abacaxi", "uva", "maca", "laranja", "limao", "melancia", "melao", "kiwi",
+    "abacate", "mamao", "pessego", "ameixa", "figo", "maracuja", "caju", "castanha", "noz", "avela",
+    "pistache", "amendoim", "cafe", "cha", "chocolate", "leite", "suco", "refrigerante", "agua", "cerveja",
+    "vinho", "whisky", "rum", "vodka", "tequila", "gin", "martini", "vermute", "sake", "conhaque",
+    "churrasco", "sushi", "pizza", "hamburguer", "sanduiche", "taco", "kebab", "curry", "sorvete", "bolo"
+    ]    
